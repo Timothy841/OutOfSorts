830e3428de06005059026e8f1d96862e3a8cbac6
Timothy <>
2020-12-29 14:41:49 -0500

    Finished insertion method and tested everything.

--- Sorts.java
+++ Sorts.java
@@ -23,4 +23,33 @@ public class Sorts{
       //System.out.println(Arrays.toString(data));
     }
   }
+
+  public static void selectionSort(int[] data){
+    for (int i = 0 ; i< data.length ; i++){
+      int a = i;
+      for (int j = i; j< data.length ; j++){
+        if (data[j]<data[a]){
+          a = j;
+        }
+      }
+      if (a != i){
+        int b = data[i];
+        data[i] = data[a];
+        data[a] = b;
+      }
+      //System.out.println(Arrays.toString(data));
+    }
+  }
+
+  public static void insertionSort(int[] data){
+    for (int i = 1; i<data.length;i++){
+      int a = data[i];
+      int j = i-1;
+      while(j>=0 && data[j]>a){
+        data[j+1] = data[j];
+        j--;
+      }
+      data[j+1] = a;
+    }
+  }
 }
